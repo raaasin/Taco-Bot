4cc92925ff4933fdab977a5e09e39713acdca28f
raaasin <nisarvskp@gmail.com>
2023-06-12 09:44:15 +0530

    adding new features

--- tour.py
+++ tour.py
@@ -1,5 +1,33 @@
 import pandas as pd
 import math
+import os
+
+def is_bracket_generated(tournament_name):
+    if tournament_name[-19:] == "_brac_generated.csv":
+        return True
+    else:
+        return False
+
+def does_tournament_exist():
+    existing_tournaments = [file for file in os.listdir() if file.startswith('to_')]
+    if existing_tournaments:
+        return "".join(existing_tournaments)
+    else:
+        return False
+    
+def tourneybrac():
+    try:
+        if is_bracket_generated(does_tournament_exist()):
+            return True
+        else:
+            return False
+    except:
+        return False
+def tourneyexists():
+    if does_tournament_exist(): 
+        return True
+    else:
+        return False
 
 def extract_team_names_from_csv(csv_file):
     colnames = ['teamname', 'player1', 'player2', 'player3', 'sub']
@@ -7,14 +35,29 @@ def extract_team_names_from_csv(csv_file):
     team_names = df['teamname'].tolist()
     return team_names
 
-def gentourney():
-    #generate tourney with tourney name, and create a database like a csv to store team and players 
-    #if tourney exists tell tourney exists and use /tourney end 
-    pass
+def gentourney(tourname):
+    tournament_csv = f"to_{tourname}.csv"
 
-def generate_double_elimination_bracket(team_names,tourney):
+    # Check if any existing tournament CSV files begin with 'to_'
+    existing_tournaments = [file for file in os.listdir() if file.startswith('to_')]
+    if existing_tournaments:
+        print(existing_tournaments)
+        return "Another tournament is already in progress. Please end the current tournament using the '/tourney end' command."
+
+    # Create an empty CSV file for the tournament
+    with open(tournament_csv, "w") as file:
+        file.write("teamname,player1,player2,player3,sub\n")
+
+    return f"Tournament '{tourname}' created successfully."
+
+def generate_double_elimination_bracket(team_names):
     #check if tourney exists to generate
-    team_names = extract_team_names_from_csv('teams.csv')
+    existing_tournaments = [file for file in os.listdir() if file.startswith('to_')]
+    if len(existing_tournaments)<1:
+        return "There is no existing tournament, Please use /tourney generate to create a tournament"
+    else:
+        tourn="".join(existing_tournaments)
+    team_names = extract_team_names_from_csv(tourn)
     num_teams = len(team_names)
     num_rounds = int(math.ceil(math.log2(num_teams)))
     num_matches = int(math.pow(2, num_rounds - 1))
@@ -67,7 +110,7 @@ def update_match_result(bracket, match_id, winner):
             updated_losers_bracket.append(match)
     bracket['losers_bracket'] = updated_losers_bracket
 
-def regtourney():
+def regtourney(player1,player2,player3,sub=None):
     #register for tourney, 
     #check if tourney exists
     #check if all players are registered users 
@@ -79,3 +122,9 @@ def unregtourney():
     #check if tourney exists
     #check if bracket generated already (no unreg now)
     pass
+
+
+
+#Test
+#print(tourneybrac())
+#print(tourneyexists())
