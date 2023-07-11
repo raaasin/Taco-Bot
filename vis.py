ac976e05fe903cf2fe11bc97b70555641438d18c
raaasin <nisarvskp@gmail.com>
2023-07-11 22:36:58 +0530

    spelling

--- vis.py
+++ vis.py
@@ -1,7 +1,7 @@
 import tour
 def vis():
     data=tour.extract_team_names_from_csv()
-    #this function visualizes the tournament in the form of an image in a particular channel
+    #this function visualizes the tournament in the from of an image in a particular channel
 def rem():
     pass
     #this function remomves the particular image from the particular channel
