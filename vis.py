b936c7f77aefc77ea6307b989eea4bdc7088240c
raaasin <nisarvskp@gmail.com>
2023-07-11 22:36:14 +0530

    commenting

--- vis.py
+++ vis.py
@@ -1,7 +1,9 @@
 import tour
 def vis():
     data=tour.extract_team_names_from_csv()
+    #this function visualizes the tournament in the form of an image in a particular channel
 def rem():
     pass
+    #this function remomves the particular image from the particular channel
 def update():
     pass
